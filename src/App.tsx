f41f2f683a303fd9fb0ca5876ff02f8d819073e0
Carlos Cabrera Gentile <carlos.cabrera@globant.com>
2021-02-23 09:49:28 -0300

    todo_list_mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Body } from './components/Body';
+import { ErrorBoundary } from './videos/ErrorBoundary/ErrorBoundary';
+import { Header } from './components/Header';
+import { TodoList } from './videos/TodoList/TodoList';
+import { TodoStore } from './videos/TodoList/TodoStore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Body>
+        <ErrorBoundary>
+          <TodoList todoStore={TodoStore} />
+        </ErrorBoundary>
+      </Body>
+    </>
   );
 }
 
